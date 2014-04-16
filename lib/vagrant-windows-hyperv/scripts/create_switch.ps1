6c60b072fb425518f30969e9e6e03ae6cc13da78
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-04-16 11:55:43 +0530

    Fix for add switch to VM.
    Find a swith with name and the swith type and add the same to the VM.

--- lib/vagrant-windows-hyperv/scripts/create_switch.ps1
+++ lib/vagrant-windows-hyperv/scripts/create_switch.ps1
@@ -40,7 +40,8 @@ try {
 
    # Add the switch to the VM's network adapter
    $vm = Get-VM -Id $vm_id -ErrorAction "stop"
-   Get-VMNetworkAdapter -VM $vm | Connect-VMNetworkAdapter -SwitchName "$name" -ErrorAction "stop"
+    Get-VMSwitch "$name" | Where-Object { $_.SwitchType -eq "$type" } `
+    | Connect-VMNetworkAdapter -VMName $vm.Name
 
    $resultHash = @{
      message = "OK"
